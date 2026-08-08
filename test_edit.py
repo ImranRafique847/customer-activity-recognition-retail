450d57f324e9bf50fc5e4857c13c26c3a8d41a25
ImranRafique847 <iamimranrafique@gmail.com>
2026-08-08 17:10:33 +0500

    Initial project commit

---
+++ test_edit.py
@@ -0,0 +1,5 @@
+# Test file - you should be able to edit this directly in Antigravity IDE
+# Double-click this file in the left sidebar to open it
+# Then try typing something below this line
+
+message = "Hello from Antigravity!"
